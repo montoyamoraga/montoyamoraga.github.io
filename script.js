68bda3bd41c2932d89b264455f5686155af5ee9b
aarón montoya-moraga <montoyamoraga@gmail.com>
2024-07-25 16:34:41 -0400

    agregar borrador multi lenguaje

---
+++ script.js
@@ -0,0 +1,40 @@
+let english = document.getElementById('english');
+let espanol = document.getElementById('espanol');
+
+english.addEventListener('click', () => {
+  changeLanguage('en');
+});
+
+espanol.addEventListener('click', () => {
+  changeLanguage('es');
+});
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const lang = localStorage.getItem('language') || 'en';
+  const langData = await fetchLanguageData(lang);
+  updateContent(langData);
+});
+
+function updateContent(langData) {
+  document.querySelectorAll('[data-i18n]').forEach((element) => {
+    const key = element.getAttribute('data-i18n');
+    element.innerHTML = langData[key];
+  });
+}
+
+function setLanguagePreference(lang) {
+  localStorage.setItem('language', lang);
+  location.reload();
+}
+
+async function fetchLanguageData(lang) {
+  const response = await fetch(`languages/${lang}.json`);
+  return response.json();
+}
+
+async function changeLanguage(lang) {
+  await setLanguagePreference(lang);
+
+  const langData = await fetchLanguageData(lang);
+  updateContent(langData);
+}
