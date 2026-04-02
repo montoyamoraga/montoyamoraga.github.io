addddbb038b9106bd7a5c13db6b0e3ac42ba0605
aarón montoya-moraga <montoyamoraga@gmail.com>
2026-04-02 18:06:44 -0300

    agregar parser

--- yaml-parser.js
+++ yaml-parser.js
@@ -2,11 +2,15 @@ fetch('./data/bio.yaml')
   .then((response) => response.text())
   .then((text) => {
     const data = jsyaml.load(text);
-    console.log(data);
+    // console.log(data);
+    // console.log(data.bio);
+    // console.log(data.bio.nombre);
+    console.log(data.bio.estudios);
 
     // document.body.innerHTML += `<h1>${data.title}</h1>`;
 
-    data.bio.forEach((item) => {
-      //   document.body.innerHTML += `<p>${item.name} (${item.type})</p>`;
+    data.bio.estudios.forEach((item) => {
+      console.log(item);
+      //   //   document.body.innerHTML += `<p>${item.name} (${item.type})</p>`;
     });
   });
